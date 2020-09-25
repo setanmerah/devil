996871b2fcdcbe1f563c57b949bd932fafc2de1f
setanmerah <71894304+setanmerah@users.noreply.github.com>
2020-09-26 02:08:36 +0900

    Add files via upload

---
+++ teste.sh
@@ -0,0 +1,15 @@
+#!/system/bin/sh
+ 
+echo "Script" pertama saya"
+echo""
+echo "Dibuat dengan bahasa Shell"
+echo""
+echo "Belajar termux"
+sleep 5 #!/system/bin/sh
+ 
+echo "Script" pertama saya"
+echo""
+echo "Dibuat dengan bahasa Shell"
+echo""
+echo "Belajar termux"
+sleep 5z
